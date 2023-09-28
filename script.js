26e54280c56fcf8d929a01901c65369f470ba68c
Kartikluhar <137763617+Kartikluhar@users.noreply.github.com>
2023-09-28 11:11:50 +0530

    updated

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let colorBox = document.getElementById("color-box")
 let inputColor = document.getElementById("input-color")
 // let taxtarea = document.getElementById("textarea")
 let textarea = document.getElementById("textarea")
+let copyShow = document.getElementById("copy-show")
+
 
 function changeColor(){
     let color = `${inputColor.value}`;
@@ -12,5 +14,9 @@ function changeColor(){
 function copycase(){
     textarea.select()
     navigator.clipboard.writeText(textarea.value)
-
+    copyShow.style.display = "block"
+    setTimeout(() => {
+        copyShow.style.display = "none"
+        // console.log("ok")
+    }, 1500);
 }
