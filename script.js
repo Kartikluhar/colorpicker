0f82835b3ede785669aff5704a8796ffff4bdc89
Kartikluhar <137763617+Kartikluhar@users.noreply.github.com>
2023-11-27 11:50:42 +0530

    done

--- script.js
+++ script.js
@@ -3,20 +3,36 @@ let inputColor = document.getElementById("input-color")
 // let taxtarea = document.getElementById("textarea")
 let textarea = document.getElementById("textarea")
 let copyShow = document.getElementById("copy-show")
+let copycolorShow = document.getElementById("copy-show-color")
 
 
 function changeColor(){
     let color = `${inputColor.value}`;
     colorBox.style.background = color;
-    textarea.innerHTML = `background-color: ${color};`
+    textarea.innerHTML = `background-color: ${color};
+color: ${color}`
 }
 
-function copycase(){
+function copybackgroundcase(){
+    let color = `${inputColor.value}`;
+    let background_color = `background-color: ${color}`
     textarea.select()
-    navigator.clipboard.writeText(textarea.value)
+    navigator.clipboard.writeText(background_color)
     copyShow.style.display = "block"
     setTimeout(() => {
         copyShow.style.display = "none"
+        // console.log("ok")
+    }, 1500);
+}
+
+function copycolorcase(){
+    let color = `${inputColor.value}`;
+    let _color = `color: ${color}`
+    textarea.select()
+    navigator.clipboard.writeText(_color)
+    copycolorShow.style.display = "block"
+    setTimeout(() => {
+        copycolorShow.style.display = "none"
         // console.log("ok")
     }, 1500);
 }
